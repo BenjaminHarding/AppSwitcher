1d1d61cff9a91e98bd6b45a4d728a4bef7fe4e4c
BenjaminHarding <benjamin.p.harding@gmail.com>
2016-05-13 13:56:34 +1200

    Dragging working

--- src/scripts/app-switcher-dnd.js
+++ src/scripts/app-switcher-dnd.js
@@ -1,6 +1,24 @@
 (function($, window) {
 
-  var dragElements = ['.js-drag-app-group', '.js-drag-app'];
+  var dragElements = ['js-drag-app-group', 'js-drag-app'];
+
+  var addGroupDragEvents = function( element ) {
+    element.addEventListener('dragenter', dragEvents.enter, false);
+    element.addEventListener('dragleave', dragEvents.leave, false);
+    element.addEventListener('dragover', dragEvents.over, false);
+    element.addEventListener('drop', dragEvents.drop, false);
+  };
+
+  var addAppDragEvents = function( element ) {
+
+    element.addEventListener('dragstart', dragEvents.started, false);
+    element.addEventListener('dragover', dragEvents.over, false);
+    /*
+    element.addEventListener('dragenter', dragEvents.enter, false);
+    element.addEventListener('drop', dragEvents.drop, false);
+    */
+    element.addEventListener('dragend', dragEvents.end, false);
+  };
 
   $(document).ready(function() {
 
@@ -12,100 +30,82 @@
     //   return;
     // }
 
-    $( dragElements ).attr('draggable', true).each(function(index,element) {
-      element.addEventListener('dragstart', dragStarted, false);
-      element.addEventListener('dragenter', dragEnter, false);
-      element.addEventListener('dragover', dragOver, false);
-      element.addEventListener('dragleave', dragLeave, false);
-      element.addEventListener('drop', dragDrop, false);
-      element.addEventListener('dragend', dragEnd, false);
+    // Apps
+    $( '.' + dragElements[1] ).attr('draggable', true).each(function(index,element) {
+
+      addAppDragEvents( element );
+
+    });
+
+    // Groups
+    $( '.' + dragElements[0] ).each(function(index,element) {
+
+      addGroupDragEvents( element );
+
     });
 
   });
 
+  // Dragging Classes
   var dragClasses = {
-    dragging: 'app-switcher-app--dragging'
+    dragging: 'app-switcher-app--dragging',
+    enter: 'app-group__content--over'
   };
 
+  // Drag Events
   var dragEvents = {
-    currentElement = null,
+    currentElement: null,
     started: function( event ) {
-
       dragEvents.currentElement = this;
+
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('text/html', this.outerHTML);
+
       $(this).addClass( dragClasses.dragging );
-
-      event.dataTransfer.effectAllowed = 'move';  // Restricts the type of drag the user can make
-      event.dataTransfer.setData('text/html', this.innerHTML);
-
     },
+
+    end: function(event) {
+      $(this).removeClass( dragClasses.dragging );
+    },
+
     enter: function( event ) {
-
+      $(this).addClass( dragClasses.enter );
     },
+
     over: function( event ) {
-
+      if (event.preventDefault) {
+        event.preventDefault();
+      }
+      event.dataTransfer.dropEffect = 'move';
+      return false;
     },
+
     leave: function( event ) {
-
+      $(this).addClass( dragClasses.enter );
     },
+
     drop: function( event ) {
+      if (event.stopPropagation) {
+        event.stopPropagation();
+      }
 
+      if( dragEvents.currentElement !== this) {
+        //Switch the element we dragged for this one
+        //dragEvents.currentElement.innerHTML = this.innerHTML;
+        $( dragEvents.currentElement ).remove();
+
+        //Create element, add to dom, append elements
+        var $element = $( event.dataTransfer.getData('text/html') )[1];
+        $( $element ).appendTo( $(this) );
+        addAppDragEvents( $element );
+
+      }
+
+      $(this).removeClass( dragClasses.enter );
+
+      return false;
     }
   };
 
-  var dragStarted = function(event) {
-
-    $(event.currentTarget).addClass('app-switcher-app--dragging');
-
-    dragElement = this;
-    //set the data & effect
-    event.dataTransfer.effectAllowed = 'move';  //restricts the type of drag the user can make
-    event.dataTransfer.setData('text/html', this.innerHTML);
-  };
-
-  var dragEnter = function(event) {
-
-    $(event.currentTarget).addClass('app-switcher-app--drag-over');
-
-  };
-
-  var dragOver = function(event) {
-    if (event.preventDefault) {
-      event.preventDefault(); // Necessary. Allows us to drop.
-    }
-
-    event.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-    return false;
-
-  };
-
-  var dragLeave = function(event) {
-
-    $(event.currentTarget).removeClass('app-switcher-app--drag-over');
-
-  };
-
-  var dragDrop = function(event) {
-    if (event.stopPropagation) {
-      event.stopPropagation(); // stops the browser from redirecting.
-    }
-
-    //Id the elements match
-    if( dragElement !== this) {
-      //Switch the element we dragged for this one
-      dragElement.innerHTML = this.innerHTML;
-      this.innerHTML = event.dataTransfer.getData('text/html');
-    }
-
-    $(this).removeClass('app-switcher-app--drag-over');
-
-    return false;
-  };
-
-  var dragEnd = function(event) {
-
-    $(event.currentTarget).removeClass('app-switcher-app--dragging');
-
-  };
 
 })($, window);
